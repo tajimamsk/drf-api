2da899c51be4c3c9733803d2ce18a40b16b60cd9
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-16 14:14:21 +0900

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .models import Task
+from rest_framework import viewsets
+from .serializers import TaskSerializer, UserSerializer
+from .ownpermissions import ProfilePermission
+
+
+# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (ProfilePermission,)
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
